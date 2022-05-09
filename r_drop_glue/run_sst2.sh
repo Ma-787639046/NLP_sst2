c8069e4516cc0775c1a926abe044a8b2f9965ab7
Ma-787639046 <Ma787639046@outlook.com>
2022-05-09 13:56:26 +0800

    push

--- r_drop_glue/run_sst2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MODEL_DIR_NAME=huggingface_bert_base_uncased_rdrop
-
-python run.py --pretrain_model_dir_name=$MODEL_DIR_NAME \
-                --run_custumized_task=sst2 \
-                --repeated_times=8 \
-                |& tee sst2.log
